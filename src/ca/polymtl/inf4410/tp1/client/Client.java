a87984cb5851dd29054017fb678830eacd077c6e
Shaeli <eleonore.carpentier@insa-cvl.fr>
2017-09-08 03:17:34 +0200

    Add files via upload
    Base pour debut de projet, les fonctions en commentaires sont la a titre indicatif. Pour lancer le client : ./client adresse_ip (127.0.0.1 pour un appel local)

---
+++ src/ca/polymtl/inf4410/tp1/client/Client.java
@@ -0,0 +1,77 @@
+package ca.polymtl.inf4410.tp1.client;
+
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import ca.polymtl.inf4410.tp1.shared.ServerInterface;
+
+public class Client {
+	public static void main(String[] args) {
+		String distantHostname = null;
+
+		if (args.length > 0) {
+			distantHostname = args[0];
+		}
+
+		Client client = new Client(distantHostname);
+		client.run();
+	}
+
+	
+	private ServerInterface distantServerStub = null;
+
+	public Client(String distantServerHostname) {
+		super();
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+
+		if (distantServerHostname != null) {
+			distantServerStub = loadServerStub(distantServerHostname);
+		}
+
+	}
+
+	private void run() {
+
+		if (distantServerStub != null) {
+			appelRMIDistant();
+		}
+	}
+
+	private ServerInterface loadServerStub(String hostname) {
+		ServerInterface stub = null;
+
+		try {
+			Registry registry = LocateRegistry.getRegistry(hostname);
+			stub = (ServerInterface) registry.lookup("server");
+		} catch (NotBoundException e) {
+			System.out.println("Erreur: Le nom '" + e.getMessage()
+					+ "' n'est pas défini dans le registre.");
+		} catch (AccessException e) {
+			System.out.println("Erreur: " + e.getMessage());
+		} catch (RemoteException e) {
+			System.out.println("Erreur: " + e.getMessage());
+		}
+
+		return stub;
+	}
+
+	// private void appelRMIDistant() {
+	// 	try{
+	// 	int result = distantServerStub.execute(4, 7);
+	// 	System.out.println("Résultat appel normal: " + result);
+	// 	} catch (RemoteException e) {
+	// 		System.out.println("Erreur: " + e.getMessage());
+	// 	}
+		
+	// }
+
+
+
+}
